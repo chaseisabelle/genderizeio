a9d5d50a58e504828fc42599931dafb66517eec1
chase <chase@life360.com>
2019-01-10 18:54:01 -0800

    tested and approved

---
+++ genderizer_test.go
@@ -0,0 +1,127 @@
+package genderizer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGenderize_success_singleName(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		response.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+		response.Write([]byte(`[{"name":"chase","gender":"male","probability":0.96,"count":296}]`))
+
+		response.WriteHeader(http.StatusOK)
+	}))
+
+	defer server.Close()
+
+	endpoint = server.URL
+
+	genders, err := Genderize("chase")
+
+	if err != nil {
+		t.Error(err)
+
+		return
+	}
+
+	count := len(genders)
+
+	if count != 1 {
+		t.Errorf("Expected 1, but got %+v.", count)
+	}
+}
+
+func TestGenderize_success_multipleNames(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		response.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+		response.Write([]byte(`[{"name":"chase","gender":"male","probability":0.96,"count":296},{"name":"isabelle","gender":"female","probability":1,"count":296}]`))
+
+		response.WriteHeader(http.StatusOK)
+	}))
+
+	defer server.Close()
+
+	endpoint = server.URL
+
+	genders, err := Genderize("chase", "isabelle")
+
+	if err != nil {
+		t.Error(err)
+
+		return
+	}
+
+	count := len(genders)
+
+	if count != 2 {
+		t.Errorf("Expected 2, but got %+v.", count)
+	}
+}
+
+func TestGenderize_failure_noNames(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		t.Errorf("Server should not have been queried.")
+	}))
+
+	defer server.Close()
+
+	endpoint = server.URL
+
+	genders, err := Genderize()
+
+	if err == nil {
+		t.Errorf("Expected error.")
+	}
+
+	if genders != nil {
+		t.Errorf("Expected nil.")
+	}
+}
+
+func TestGenderize_failure_emptyName(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		t.Errorf("Server should not have been queried.")
+	}))
+
+	defer server.Close()
+
+	endpoint = server.URL
+
+	genders, err := Genderize("chase", "")
+
+	if err == nil {
+		t.Errorf("Expected error.")
+	}
+
+	if genders != nil {
+		t.Errorf("Expected nil.")
+	}
+}
+
+func TestGenderize_failure_error(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		response.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+		response.Write([]byte(`{"error":"something terrible has happened"}`))
+
+		response.WriteHeader(http.StatusBadRequest)
+	}))
+
+	defer server.Close()
+
+	endpoint = server.URL
+
+	genders, err := Genderize("chase", "isabelle")
+
+	if err == nil {
+		t.Error("Expected error.")
+	}
+
+	if genders != nil {
+		t.Error("Expected nil.")
+	}
+}
